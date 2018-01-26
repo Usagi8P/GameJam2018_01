565bf961786da6c5a9b64ccc0602a46300e10d46
Philipp Stein <ph7stein@hotmail.com>
2018-01-26 20:51:10 +0000

    Timer Code

---
+++ Transmission/Assets/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+//This script is for the "you have been detected" timer
+
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    private float startTime;
+
+	// Use this for initialization
+	void Start () {
+        startTime = 10; //at which time we want to start the timer
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if else if seen goes here
+        if (startTime - Time.time >= 0) // replace with if (seen)
+        {
+            float t = startTime - Time.time;
+
+            string seconds = t.ToString("f2");
+
+            timerText.text = seconds;
+        }
+
+        else if (startTime - Time.time <= 0)
+        {
+            timerText.text = "";
+        }
+    }
+}
