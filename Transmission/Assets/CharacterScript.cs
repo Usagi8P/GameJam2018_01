25906e2023fa64ad1862c681b603b7f34bd4e5ec
Philipp Stein <ph7stein@hotmail.com>
2018-01-27 18:15:47 +0000

    Fire Bullets

---
+++ Transmission/Assets/CharacterScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour {
+
+    public GameObject bullet;
+    public Transform bulletSpawn;
+    public float fireRate;
+    public Camera camera;
+    private float nextFire;
+    private RaycastHit hit;
+    public Vector3 tempDirection;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        hit = new RaycastHit();
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);       
+            if (Physics.Raycast(ray, out hit))
+            {
+                tempDirection = (hit.point - gameObject.transform.position).normalized;
+                nextFire = Time.time + fireRate;
+                Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+            }
+        }
+    }
+}
---
+++ Transmission/Assets/Mover_Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover_Bullet : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    private Vector3 direction;
+    public GameObject source;
+
+    public void Start()
+    {
+        direction = GameObject.Find("Cube").GetComponent<CharacterScript>().tempDirection;
+    }
+
+	// Use this for initialization
+	public void Update()
+    {
+        transform.Translate(direction);
+
+    }
+}
