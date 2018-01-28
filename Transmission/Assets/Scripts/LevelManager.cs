31ab49347889a2eee8901703c22f31b664ce114e
Philipp Stein <ph7stein@hotmail.com>
2018-01-28 12:20:45 +0000

    a try

---
+++ Transmission/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject optionsCanvas;
+    public GameObject mainCanvas;
+    public GameObject creditsCanvas;
+    public Slider volumeSlider;
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void OnOptionsPress()
+	{
+        optionsCanvas.SetActive(true);
+        mainCanvas.SetActive(false);
+	}
+
+    public void OnCreditsPress()
+    {
+        optionsCanvas.SetActive(true);
+        mainCanvas.SetActive(false);
+    }
+
+    public void OnSliderUse()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+
+	public void OnExitPress()
+	{
+        Debug.Log("Game quit requested");
+        Application.Quit();
+	}
+}
